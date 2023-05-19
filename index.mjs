64cfea5467c3c97df6590c93b0974690ab20917e
norisugi62 <noriaki197962@gmail.com>
2023-05-19 22:14:35 +0900

    7th commit.

--- index.mjs
+++ index.mjs
@@ -1,10 +1,12 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
 
-export const filename = () => {
+const fileName = () => {
   return fileURLToPath(import.meta.url);
 }
 
-export const dirname = () => {
+const dirName = () => {
   return path.dirname(fileURLToPath(import.meta.url));
 }
+
+export { fileName, dirName };
